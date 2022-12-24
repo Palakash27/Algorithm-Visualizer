7d8f512c494d25a7e0b63e9bb354014a6cc499ee
Aakash Pal <palakash27@gmail.com>
2022-12-24 04:17:35 -0500

    Add readme page on start and fix array bar color to black on highlight

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,34 @@ import Home from "./components/Home/Home";
 import ErrorPage from "./error-page";
 import SortingVisualizer from "./components/SortingVisualizer/SortingVisualizer";
 import { Routes, Route } from "react-router-dom";
+import Readme from "./components/Readme/Readme";
+import { useState } from "react";
 
 const App = () => {
+    const [isVisible, setIsVisible] = useState(true);
+
     return (
-        <div>
-            <Header />
+        <>
+            {isVisible ? (
+                <Readme onHideReadme={() => setIsVisible(false)} />
+            ) : (
+                <div>
+                    <Header />
+                    <Routes>
+                        <Route index element={<Home />} />
+                        <Route
+                            path="sorting-algorithm"
+                            element={<SortingVisualizer />}
+                        />
 
-            <Routes>
-                <Route index element={<Home />} />
-                <Route
-                    path="sorting-algorithm"
-                    element={<SortingVisualizer />}
-                />
-
-                {/* Using path="*"" means "match anything", so this route
+                        {/* Using path="*"" means "match anything", so this route
                 acts like a catch-all for URLs that we don't have explicit
                 routes for. */}
-                <Route path="*" element={<ErrorPage />} />
-            </Routes>
-        </div>
+                        <Route path="*" element={<ErrorPage />} />
+                    </Routes>
+                </div>
+            )}
+        </>
     );
 };
 
---
+++ src/components/Readme/Readme.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import { marked } from "marked";
+import "./Readme.css";
+import readmeFile from "./README.md";
+
+const Readme = ({ onHideReadme }) => {
+    const [readme, setReadme] = useState("");
+
+    useEffect(() => {
+        // Check if the readme has already been displayed
+        if (localStorage.getItem("readmeDisplayed") === "true") {
+            onHideReadme();
+        }
+    }, []);
+
+    useEffect(() => {
+        // Make an HTTP request to the server to fetch the readme.md file using the fetch API
+        fetch(readmeFile)
+            .then((response) => response.text())
+            .then((text) => {
+                // Convert the markdown text to HTML using the marked library
+                const html = marked(text);
+                setReadme(html);
+
+                // Set the readmeDisplayed flag to true
+                localStorage.setItem("readmeDisplayed", "true");
+            })
+            .catch((error) => {
+                // Handle the error
+            });
+    }, []);
+
+    return (
+        <div className="readme">
+            <hr />
+            <div dangerouslySetInnerHTML={{ __html: readme }} />
+            <div className="btn">
+                <button
+                    onClick={() => {
+                        onHideReadme();
+                        localStorage.setItem("readmeDisplayed", "true");
+                    }}
+                >
+                    Get Started
+                </button>
+            </div>
+            <hr />
+        </div>
+    );
+};
+
+export default Readme;
