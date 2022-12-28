7d42755d1bbbd0ecabc4e61b6ffe996bfe628b5d
Aakash Pal <palakash27@gmail.com>
2022-12-28 14:21:07 -0500

    Rename NoMatch component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
-import ErrorPage from "./error-page";
+import NoMatch from "./NoMatch";
 import SortingVisualizer from "./components/SortingVisualizer/SortingVisualizer";
 import { Routes, Route } from "react-router-dom";
 import Readme from "./components/Readme/Readme";
@@ -26,7 +26,7 @@ const App = () => {
                         {/* Using path="*"" means "match anything", so this route
                 acts like a catch-all for URLs that we don't have explicit
                 routes for. */}
-                        <Route path="*" element={<ErrorPage />} />
+                        <Route path="*" element={<NoMatch />} />
                     </Routes>
                 </div>
             )}
---
+++ src/NoMatch.jsx
@@ -0,0 +1,8 @@
+export default function NoMatch() {
+    return (
+        <div id="error-page">
+            <h1>Oops!</h1>
+            <p>Noting to see here</p>
+        </div>
+    );
+}
